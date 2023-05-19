f767e9b4c17522ab79f63ed361e4594844324222
Shreyas <shrepranav5@gmail.com>
2023-05-19 14:09:39 +0530

    Added Dependencies

---
+++ deps/cimgui/b/CMakeFiles/cimgui.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cimgui.dir/cimgui.cpp.obj"
+  "CMakeFiles/cimgui.dir/cimgui.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/backends/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/backends/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/imgui.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/imgui.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/imgui_demo.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/imgui_demo.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/imgui_tables.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/cimgui.dir/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/cimgui.dir/imgui/imgui_widgets.cpp.obj.d"
+  "cimgui.dll"
+  "cimgui.dll.manifest"
+  "cimgui.pdb"
+  "libcimgui.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cimgui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
